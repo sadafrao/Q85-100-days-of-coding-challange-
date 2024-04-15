0f465999ec950640fa640dd85c9dd4e8678c3499
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:23:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q85
+// this function show up in a text 
+function codeposition(str) {
+    return str.indexOf("code");
+}
+console.log(codeposition("I am a web developer and I love to code")); // it tell us aposition of code
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Q85
+// this function show up in a text 
+function codeposition(str: string): number{
+    return str.indexOf("code");
+
+}
+console.log(codeposition("I am a web developer and I love to code"));// it tell us aposition of code
